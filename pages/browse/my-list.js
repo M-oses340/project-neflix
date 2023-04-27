2ec657011955db8a5c6df01f9f41f9b9de0f0f96
M-oses340 <mosesomwa7@gmail.com>
2023-04-27 10:20:23 +0300

    SSR in Mylist

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -1,11 +1,24 @@
 import Head from "next/head";
 import NavBar from "../../components/nav/navbar";
-
+import { redirectUser } from "../../utils/redirectUser";
+import { getMyList } from "../../lib/videos";
 import SectionCards from "../../components/card/section-cards";
 import styles from "../../styles/MyList.module.css";
 
-const MyList = () => {
+export async function getServerSideProps(context) {
+    const { userId, token } = await redirectUser(context);
+    const videos = await getMyList(userId, token);
+  
+    return {
+      props: {
+        myListVideos: videos,
+      },
+    };
+  }
+  
+  const MyList = ({ myListVideos }) => {
     return (
+    
         <div>
             <Head>
               <title>My list</title>
@@ -13,7 +26,7 @@ const MyList = () => {
             <main className={styles.main}>
                <NavBar />
                 <div className={styles.sectionWrapper}>
-                  <SectionCards title="My List" videos={[]} size="small" />
+                  <SectionCards title="My List" videos={myListVideos} size="small" />
                 </div>
             </main>
          </div>
